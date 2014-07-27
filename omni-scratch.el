22b1c55926b06169c2169174667f0779acb514d4
Adriean Khisbe <adriean.khisbe@live.fr>
2014-07-27 16:01:24 +0200

    init: skeleton

---
+++ omni-scratch.el
@@ -0,0 +1,32 @@
+;;; omni-scratch.el --- Easy and mode-specific draft buffers
+
+;; Copyright (C) 2014  Adrien Becchis
+
+;; Author: Adrien Becchis <adriean.khisbe@live.fr>
+;; Created:  2014-07-27
+;; Version: 0.1
+;; Keywords: convenience, languages, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'omni-scratch)
+;;; omni-scratch.el ends here
