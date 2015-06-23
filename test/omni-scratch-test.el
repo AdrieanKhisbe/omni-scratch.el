a24d045e6479298c48aff9edafae8906c868e0fd
Adriean Khisbe <adriean.khisbe@live.fr>
2015-06-23 18:39:54 +0200

    test: scaffold real ert tests

---
+++ test/omni-scratch-test.el
@@ -0,0 +1,10 @@
+;;; Test for `omni-scratch'
+
+;;; Commentary:
+;; These are the tests for `omni-scratch'
+
+;;; Code:
+
+(ert-deftest omni-scratch-should-not-pass ()
+  (should-not nil))
+
---
+++ test/test-helper.el
@@ -0,0 +1,38 @@
+;;; test-helper --- Test helper for omni-scratch
+
+;;; Commentary:
+;; test helper inspired from https://github.com/tonini/overseer.el/blob/master/test/test-helper.el
+
+;;; Code:
+
+(require 'f)
+
+(defvar cpt-path
+  (f-parent (f-this-file)))
+
+(defvar omni-scratch-test-path
+  (f-dirname (f-this-file)))
+
+(defvar omni-scratch-root-path
+  (f-parent omni-scratch-test-path))
+
+(defvar omni-scratch-sandbox-path
+  (f-expand "sandbox" omni-scratch-test-path))
+
+(when (f-exists? omni-scratch-sandbox-path)
+  (error "Something is already in %s. Check and destroy it yourself" omni-scratch-sandbox-path))
+
+(defmacro within-sandbox (&rest body)
+  "Evaluate BODY in an empty sandbox directory."
+  `(let ((default-directory omni-scratch-sandbox-path))
+     (when (f-exists? omni-scratch-sandbox-path)
+       (f-delete default-directory :force))
+     (f-mkdir omni-scratch-sandbox-path)
+     ,@body
+     (f-delete default-directory :force)))
+
+(require 'ert)
+(require 'omni-scratch (f-expand "omni-scratch" omni-scratch-root-path))
+
+(provide 'test-helper)
+;;; test-helper.el ends here
